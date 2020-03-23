aac902059c592302a63ab736e0eb7f26dae88f8b
xiaozuzong <aslan874470891@gmail.com>
2020-03-23 15:12:03 +0800

    完成所有功能

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/my-project/'
+  ? '/shark-demo/'
   : '/',
   lintOnSave: false,
   chainWebpack: config =>{
